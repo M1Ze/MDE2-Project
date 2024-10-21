b4d9367d73e1e01fc7b8c831d37d80666d3c6bb1
M1Ze <pappel_clever0k@icloud.com>
2024-10-21 10:20:52 +0200

    venv commit

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 if __name__ == '__main__':
-    print_hi('Sebastian braucht immer Extrawürstl ')
+    print_hi('First Setup ')
