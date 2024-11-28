63da904d7012b2e1670285265105ea10680be63b
mize <pappel_clever0k@icloud.com>
2024-11-28 14:07:06 +0100

    first DB extended

--- db_models.py
+++ db_models.py
@@ -5,19 +5,22 @@ db = SQLAlchemy()
 class Patient(db.Model):
     __tablename__ = 'patient'
     id = db.Column(db.Integer, primary_key=True)
-    identifier = db.Column(db.String(100), unique=True, nullable=False)  # Unique patient identifier
+    identifier = db.Column(db.String(100), unique=True, nullable=False)  # Unique patient identifier (SVN)
     name = db.Column(db.String(200), nullable=False)  # Patient's full name
     birth_date = db.Column(db.Date, nullable=False)  # Birth date of the patient
     gender = db.Column(db.String(10), nullable=False)  # Gender of the patient ('male', 'female', 'other', 'unknown')
     address = db.Column(db.String(500))  # Address details
-    contact = db.Column(db.String(200))  # Contact information (e.g., phone number)
-    managing_organization = db.Column(db.String(200))  # Organization managing the patient's care
+    contact_phone = db.Column(db.String(200)) # phone information
+    contact_email = db.Column(db.String(200)) # mail information
+    contact_person_name = db.Column(db.String(200)) # Contact Person name
+    contact_person_phone = db.Column(db.String(200)) # Contact Person phone
 
 class HealthData(db.Model):
     __tablename__ = 'health_data'
     id = db.Column(db.Integer, primary_key=True)
     patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
-    data_type = db.Column(db.String(50), nullable=False)  # Type of FHIR resource
+    data_type = db.Column(db.String(50), nullable=False)  # Type of Oberservation
+    data_aqu_datetime = db.Column(db.DateTime, nullable=False) # aquistiton date and Time
     data = db.Column(db.Text, nullable=False)  # JSON data for FHIR resource
 
     patient = db.relationship('Patient', backref=db.backref('health_records', lazy=True))
