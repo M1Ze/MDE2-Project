65fbd23c365ff1cc10b4b2e7634b650329e31743
mize <pappel_clever0k@icloud.com>
2025-01-03 18:14:58 +0100

    added create fhire in patient

--- data_extraction/patient_data.py
+++ data_extraction/patient_data.py
@@ -1,3 +1,9 @@
+import json
+import os
+
+from fhir.resources.address import Address
+from fhir.resources.contactpoint import ContactPoint
+from fhir.resources.humanname import HumanName
 from fhir.resources.patient import Patient
 
 class PatientData:
@@ -56,3 +62,83 @@ class PatientData:
                     None,
                 )
                 self.contacts.append({"name": contact_name, "phone": contact_phone})
+
+    def create_fhire(self, filepath):
+        patient_fhire_resource = f"patient_{filename}.json"
+        full_path = os.path.join(filepath, patient_fhire_resource)
+        with open(full_path, "w") as file:
+            file.write("{}")
+
+
+    def create_fhire(self, filepath, filename):
+        # Create the FHIR Patient resource
+        patient_resource = Patient.construct()
+
+        # Populate identifier
+        if self.identifier:
+            patient_resource.identifier = [
+                {"system": "http://example.org/fhir/identifier", "value": self.identifier}
+            ]
+
+        # Populate name
+        if self.name:
+            name_parts = self.name.split(" ")
+            patient_resource.name = [
+                HumanName.construct(
+                    family=name_parts[-1], given=name_parts[:-1]
+                )
+            ]
+
+        # Populate birthdate
+        if self.birthdate:
+            patient_resource.birthDate = self.birthdate
+
+        # Populate gender
+        if self.gender:
+            patient_resource.gender = self.gender
+
+        # Populate address
+        if self.address:
+            address_parts = self.address.split(", ")
+            patient_resource.address = [
+                Address.construct(
+                    line=[address_parts[0]],
+                    city=address_parts[1] if len(address_parts) > 1 else None,
+                    state=address_parts[2] if len(address_parts) > 2 else None,
+                    postalCode=address_parts[3] if len(address_parts) > 3 else None,
+                )
+            ]
+
+        # Populate telecom
+        patient_resource.telecom = []
+        if self.phone:
+            patient_resource.telecom.append(
+                ContactPoint.construct(system="phone", value=self.phone, use="home")
+            )
+        if self.email:
+            patient_resource.telecom.append(
+                ContactPoint.construct(system="email", value=self.email, use="home")
+            )
+
+        # Populate contacts
+        if self.contacts:
+            patient_resource.contact = []
+            for contact in self.contacts:
+                contact_name = HumanName.construct(
+                    family=contact["name"].split(" ")[-1],
+                    given=contact["name"].split(" ")[:-1],
+                )
+                contact_telecom = [
+                    ContactPoint.construct(
+                        system="phone", value=contact["phone"], use="mobile"
+                    )
+                ]
+                patient_resource.contact.append(
+                    {"name": contact_name, "telecom": contact_telecom}
+                )
+
+        # Write the Patient resource to a JSON file
+        patient_fhire_resource = f"patient_{filename}.json"
+        full_path = os.path.join(filepath, patient_fhire_resource)
+        with open(full_path, "w") as file:
+            json.dump(patient_resource.dict(), file, indent=4)
